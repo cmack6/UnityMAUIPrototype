9063774d1e56a3b22928909252153d3a4ad95ce8
cmack6 <cmack6@ycp.edu>
2025-03-02 14:54:20 -0500

    copied test project and pasted into repo

---
+++ UnityPrototype/UnityPrototype/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Windowing;
+using Windows.Win32.Foundation;
+using Windows.Win32.UI.WindowsAndMessaging;
+
+namespace UnityPrototype
+{
+    public partial class MainPage : ContentPage
+    {
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        private Process _unityProcess;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            StartUnity();
+        }
+
+        private void StartUnity()
+        {
+            _unityProcess = new Process();
+            _unityProcess.StartInfo.FileName = "PathToYourUnityBuild.exe";
+            _unityProcess.StartInfo.WindowStyle = ProcessWindowStyle.Minimized;
+            _unityProcess.Start();
+            _unityProcess.WaitForInputIdle();
+
+            IntPtr unityHwnd = _unityProcess.MainWindowHandle;
+            IntPtr mauiHwnd = WinRT.Interop.WindowNative.GetWindowHandle(MainWindow.Instance);
+
+            SetParent(unityHwnd, mauiHwnd);
+        }
+    }
+}
---
+++ UnityPrototype/UnityPrototype/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace UnityPrototype
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
