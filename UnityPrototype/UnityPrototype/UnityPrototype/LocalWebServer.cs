f22b13a9f4580cf7e6a06287aca3c17c28b88d28
cmack6 <cmack6@ycp.edu>
2025-03-02 15:47:54 -0500

    updated maui stuff to have server

---
+++ UnityPrototype/UnityPrototype/UnityPrototype/LocalWebServer.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+public class LocalWebServer
+{
+    private IHost? _host;
+    private readonly string _url = "http://localhost:5000";
+
+    public void StartServer()
+    {
+        string unityBuildPath = @"C:\Users\wolfm\OneDrive\Documents\GitHub\UnityMAUIPrototype\BallBouncing";
+
+        if (!Directory.Exists(unityBuildPath))
+        {
+            throw new DirectoryNotFoundException($"The directory '{unityBuildPath}' does not exist.");
+        }
+        _host = Host.CreateDefaultBuilder()
+            .ConfigureServices(services =>
+            {
+                services.AddSingleton<IWebHost>(_ => new WebHostBuilder()
+                    .UseKestrel()
+                    .UseUrls(_url)
+                    .Configure(app =>
+                    {
+                        app.UseStaticFiles(new StaticFileOptions
+                        {
+                            FileProvider = new Microsoft.Extensions.FileProviders.PhysicalFileProvider(unityBuildPath),
+                            RequestPath = ""
+                        });
+                    })
+                    .Build());
+            })
+            .Build();
+
+        Task.Run(() => _host.Run());
+    }
+}
--- UnityPrototype/UnityPrototype/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/UnityPrototype.AssemblyInfo.cs
+++ UnityPrototype/UnityPrototype/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/UnityPrototype.AssemblyInfo.cs
@@ -11,14 +11,10 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PackageName", "com.companyname.unityprototype")]
-[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PublisherName", "User Name")]
-[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Name", "UnityPrototype")]
-[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Version", "1.0.0.1")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnityPrototype")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnityPrototype")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnityPrototype")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
